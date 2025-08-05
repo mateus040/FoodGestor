317e04af6d724e552ca08499ceb4d00c97a802a6
mateus040 <mateusgabrielmoreno264@gmail.com>
2025-08-05 16:34:04 -0300

    Adicionar arquivos de projeto.

---
+++ FoodGestor/Context/FoodGestorContext.cs
@@ -0,0 +1,14 @@
+﻿using FoodGestor.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FoodGestor.Context
+{
+    public class FoodGestorContext : DbContext
+    {
+        public FoodGestorContext(DbContextOptions<FoodGestorContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category>? Categories { get; set; }
+    }
+}
---
+++ FoodGestor/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace FoodGestor.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
